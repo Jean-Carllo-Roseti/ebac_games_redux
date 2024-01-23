9459e677b085080e931df7737a827f228d1c37f3
jean carllo <jeanrosete04@gmail.com>
2024-01-23 14:22:40 -0300

    n consegui utilizar o 'delay 1000', gostaria de uma ajuda.

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import carrinhoRecuder from './reducers/carrinho'
 
+import api from '../services/api'
+import { buildGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware'
+
 export const store = configureStore({
   reducer: {
-    carrinho: carrinhoRecuder
-  }
+    carrinho: carrinhoRecuder,
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (buildGetDefaultMiddleware) =>
+    buildGetDefaultMiddleware().concat(api.middleware)
 })
 
 export type RootReducer = ReturnType<typeof store.getState>
